a3cda6bf922775efc8a55c896a0fe3f306f23a41
Bijan Sabbagh <bijans@gmail.com>
2023-10-16 18:11:58 +0200

    transformations finished

--- collider.js
+++ collider.js
@@ -1,22 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Your JavaScript code here
-  let collapseContent = document.getElementsByClassName("coll-content-1")[0];
-  // Collapse Header (toggle button)
-  let collapseHeader = document.getElementsByClassName("dropdowna")[0];
-  collapseContent.style.visibility = "hidden";
+  const accordion_titles = document.querySelectorAll('.frame11 .dropdowna');
 
-  // Custom JavaScript (You can Implement it in a better way!) (ES6)
-  // On Click Event the Header
-  collapseHeader.addEventListener("click", () => {
-    // For Toggling we need to know the current state of the collapse then change it
-    if (collapseContent.style.visibility === "hidden") {
-      // Collapse is already hidden, then show it
-      collapseContent.style.visibility = "visible";
-      collapseContent.style.opacity = "1";
-    } else {
-      // Else, hide the collapse
-      collapseContent.style.visibility = "hidden";
-      collapseContent.style.opacity = "0";
-    }
+  accordion_titles.forEach((at) => {
+    at.addEventListener("click", ()=>{
+      at.parentElement.classList.toggle('accordion-is-open')
+      const ac = at.parentElement.querySelector('.accordion-content');
+      if (ac.style.maxHeight) {
+        ac.style.maxHeight = null;
+      } else {
+        ac.style.maxHeight = ac.scrollHeight + "px";
+      }
+    });
   });
 });
